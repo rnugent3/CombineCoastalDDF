d897b87fe5298530883cb055c9e35fd9f8d1b289
rnugent3 <richard.nugent.3@gmail.com>
2021-02-12 14:12:13 -0800

    triangular code added

---
+++ hello.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	var t = TriangularDistribution{
+		Min:        0,
+		MostLikely: 10,
+		Max:        30,
+	}
+	fmt.Println(fmt.Sprintf("Triangular Distribution is %f", t.InvCDF(.5)))
+}
+
+type TriangularDistribution struct {
+	Min        float64
+	MostLikely float64
+	Max        float64
+}
+
+func (t TriangularDistribution) InvCDF(probability float64) float64 {
+	a := t.MostLikely - t.Min
+	b := t.Max - t.MostLikely
+	if probability <= 0 {
+		return t.Min
+	} else if probability < a/(t.Max-t.Min) {
+		return t.Min + math.Sqrt(probability*(t.Max-t.Min)*a)
+	} else if probability < 1 {
+		return t.Max - math.Sqrt((1-probability)*(t.Max-t.Min)*b)
+	}
+	return t.Max
+}
+func (t TriangularDistribution) CDF(value float64) float64 {
+	if value < t.Min {
+		return 0
+	} else if value <= t.MostLikely {
+		return math.Pow((value-t.Min), 2) / ((t.Max - t.Min) * (t.MostLikely - t.Min))
+	} else if value <= t.Max {
+		return 1 - math.Pow((t.Max-value), 2)/((t.Max-t.Min)*(t.Max-t.MostLikely))
+	}
+	return 1
+}
+func (t TriangularDistribution) PDF(value float64) float64 {
+	if value < t.Min {
+		return 0
+	} else if value <= t.MostLikely {
+		return 2 * (value - t.Min) / ((t.Max - t.Min) * (t.MostLikely - t.Min))
+	} else if value <= t.Max {
+		return 2 * (t.Max - value) / ((t.Max - t.Min) * (t.Max - t.MostLikely))
+	} else {
+		return 0
+	}
+}
+func (t TriangularDistribution) CentralTendency() float64 {
+	return t.MostLikely
+}
